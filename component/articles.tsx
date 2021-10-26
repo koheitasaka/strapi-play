a8782d67f06939f03433cffa43bcae2bd3804f5e
koheitasaka <soundasleep@keio.jp>
2021-10-27 02:16:03 +0900

    create articles and card component

---
+++ component/articles.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from './card';
+
+const Articles = ({ articles }: any) => {
+  const leftArticlesCount = Math.ceil(articles.length / 5);
+  const leftArticles = articles.slice(0, leftArticlesCount);
+  const rightArticles = articles.slice(leftArticlesCount, articles.length);
+
+  return (
+    <div>
+      <div className="uk-child-width-1-2@s" data-uk-grid="true">
+        <div>
+          {leftArticles.map((article: any, i: number) => {
+            return (
+              <Card article={article} key={`article__left__${article.slug}`} />
+            );
+          })}
+        </div>
+        <div>
+          <div className="uk-child-width-1-2@m uk-grid-match" data-uk-grid>
+            {rightArticles.map((article: any, i: number) => {
+              return (
+                <Card
+                  article={article}
+                  key={`article__left__${article.slug}`}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Articles;
